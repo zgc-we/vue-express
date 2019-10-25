744e6929f07487735bc8144a090e5f338b970087
ludan <m15133077581@163.com>
2019-10-25 09:20:55 +0800

    图片修改，采用引用路径

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-let indexListData = require('../public/data/index-list.json')
+let indexListData = require('../public/data/index-list.json');
+let titleList = require('../public/data/titleList.json');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//列表list 
 router.get('/api/indexList', function(req, res, next) {
   res.json({
     code: 0,
     data: indexListData
+  })
+});
+// header title list列表
+router.get('/api/titleList', function(req, res, next) {
+  res.json({
+    code: 0,
+    list: titleList
   })
 });
 
